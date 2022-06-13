f722c8e1eecb0fe1b4db0cb86fea0de71dec43e7
Андрей Александрович <res1.main@gmail.com>
2022-06-13 12:44:31 +0300

    refactor

--- core-navigation-impl/src/main/java/com/ozontech/core_navigation_impl/MainActivity.kt
+++ core-navigation-impl/src/main/java/com/ozontech/core_navigation_impl/MainActivity.kt
@@ -4,15 +4,4 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.ozontech.feature_products_api.ProductNavigationApi
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-	companion object : ProductNavigationApi {
-		override fun navigateToPDP(fragment: Fragment, guid: String) {
-
-		}
-
-		override fun navigateToAdd(fragment: Fragment) {
-
-		}
-	}
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
