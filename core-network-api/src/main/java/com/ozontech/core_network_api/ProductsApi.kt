42eaa45188fe28d1d9d6e582b3d1a78f1f4ca3da
Андрей Александрович <res1.main@gmail.com>
2022-06-27 00:02:48 +0300

    sharing gradle configuration

--- core-network-api/src/main/java/com/ozontech/core_network_api/ProductsApi.kt
+++ core-network-api/src/main/java/com/ozontech/core_network_api/ProductsApi.kt
@@ -9,10 +9,7 @@ interface ProductsApi {
 	@GET("50afcd4b-d81e-473e-827c-1b6cae1ea1b2")
 	suspend fun getProductsInList(): List<ProductInListDto>
 
-	suspend fun incrementCounter(guid: String)
-
 	@GET("8c374376-e94e-4c5f-aa30-a9eddb0d7d0a")
 	suspend fun getProducts(): List<ProductDto>
 
-	suspend fun addRandomProduct()
 }
